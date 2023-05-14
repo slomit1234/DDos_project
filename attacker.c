601b8a4522eed6d92d3af2ec754227f19b332e31
slomit1234 <42152443+slomit1234@users.noreply.github.com>
2023-05-14 17:34:36 +0300

    Update attacker.c

--- attacker.c
+++ attacker.c
@@ -111,3 +111,27 @@ int main(int argc, char *argv[])
     fclose(f);
     
 }
+
+unsigned short csum(unsigned short *ptr, int nbytes)
+{
+    register long sum;
+    unsigned short oddbyte;
+    register short answer;
+
+    sum = 0;
+    while (nbytes > 1) {
+        sum += *ptr++;
+        nbytes -= 2;
+    }
+    if (nbytes == 1) {
+        oddbyte = 0;
+        *((unsigned char *) &oddbyte) = *(unsigned char *)ptr;
+        sum += oddbyte;
+    }
+
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    answer = (short)~sum;
+
+    return answer;
+}
