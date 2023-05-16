82a94366cee13386a684aac8ce281f025563cbf3
slomit1234 <42152443+slomit1234@users.noreply.github.com>
2023-05-16 09:56:45 +0300

    Add files via upload

--- apache_victim.py
+++ apache_victim.py
@@ -5,6 +5,6 @@ PORT = 80
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
     print("Serving at port", PORT)
     httpd.serve_forever()
